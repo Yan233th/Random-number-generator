2605a2d021e1cc5073b907925833422321a2721c
Yan233_ <Yan233th@126.com>
2023-05-20 21:23:08 +0800

    2

--- GUI/MainWindow.xaml.cs
+++ GUI/MainWindow.xaml.cs
@@ -42,5 +42,10 @@ namespace GUI
         {
             WindowState = WindowState.Minimized;
         }
+
+        private void Button_Click (object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
